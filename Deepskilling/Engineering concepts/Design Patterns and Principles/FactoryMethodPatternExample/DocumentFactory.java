0a88d30225804b713683f746f0e7b942a5a9c8eb
Laahiri31 <laahiri31@gmail.com>
2026-06-25 22:42:33 +0530

    Factory,Singleton

---
+++ Deepskilling/Engineering concepts/Design Patterns and Principles/FactoryMethodPatternExample/DocumentFactory.java
@@ -0,0 +1,11 @@
+package FactoryMethodPatternExample;
+
+public abstract class DocumentFactory {
+
+    public abstract Document createDocument();
+
+    public void displayDocument() {
+        Document document = createDocument();
+        document.open();
+    }
+}
